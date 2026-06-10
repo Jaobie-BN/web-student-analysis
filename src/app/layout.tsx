c3875b5d62262ba38b31de6b3dcd43b37a8c3803
Jaobie-BN <teerapatkemtis@gmaail>
2026-06-10 15:27:39 +0700

    Initial commit with student analysis project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { ClassroomProvider } from "@/context/ClassroomContext";
+import { Analytics } from "@vercel/analytics/next"
+import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-background text-foreground">
         <ClassroomProvider>
           {children}
+          <Analytics />
         </ClassroomProvider>
       </body>
     </html>
